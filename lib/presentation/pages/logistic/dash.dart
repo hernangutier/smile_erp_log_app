2f3cf90a6ca4515e72f183cd3274a2d74bfbacb2
hernangutier <hernangutier@gmail.com>
2026-03-27 11:35:26 -0400

    Organizando Structure Proyect

---
+++ lib/presentation/pages/logistic/dash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+//  DasBoard Logistica
+
+class DashBoard extends StatelessWidget {
+  const DashBoard({super.key});
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
